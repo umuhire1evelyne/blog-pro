258d483863a72cde5b2f5b46adbb48f5a244b190
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-30 15:21:32 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:ineza@localhost/watchlist_test'
+
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'uevelyne44@gmail.com'
+
+# simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+
+class ProdConfig(Config):
+   
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:ineza@localhost/watchlist_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:ineza@localhost/watchlist'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY='0a05ea57a37e91924e9ee789ef7f8fa9'
+export SECRET_KEY=Flask WTF Secret Key
+export MAIL_USERNAME=uevelyne44@gmail.com
+export MAIL_PASSWORD=boubouni
+python3.6 manage.py server
